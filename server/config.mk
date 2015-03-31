1eecad5ec2be26070eaa9a1957341836a2dd0bd2
Blackrush <blackrushx@gmail.com>
2015-03-31 17:29:15 +0200

    Improve Makefile

---
+++ server/config.mk
@@ -0,0 +1,27 @@
+# Name of the generated executable binary.
+NAME    = server
+
+# Directory containing all C sources
+SRC_DIR = src
+
+# Directory containing all C headers
+INC_DIR = include
+
+# Directory containing all generated objects.
+OUT_DIR = _build
+
+# The compiler to use.
+CC      = gcc
+
+# The linker to use.
+LD      = gcc
+
+# The list of dependencies to use.
+CDEPS   = 
+
+# Custom flags given to the compiler.
+CFLAGS  = -O2 -Wall
+
+# Custom flags given to the linker.
+LDFLAGS =
+
