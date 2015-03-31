2e9af654b033eea8458045ddfb075b47c5a18e4a
Blackrush <blackrushx@gmail.com>
2015-03-31 18:14:08 +0200

    Add comments around the code

--- server/src/main.c
+++ server/src/main.c
@@ -7,51 +7,72 @@
 #include "server.h"
 
 int main(int argc, char **argv) {
-	int                sock, n;
+	int                sock;
 	struct sockaddr_in sin, csin;
-	socklen_t          csin_len;
 	client_t           client;
+
+	int                n;
 	char               *buf, *data;
 	error_t            err;
 
+	// Create a socket.
 	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (-1 == sock) {
 		perror("couldnt create socket");
 		return 1;
   }
 
+	// Configure it.
 	sin.sin_family      = AF_INET;
 	sin.sin_port        = htons(PORT);
 	sin.sin_addr.s_addr = htonl(INADDR_ANY);
-	csin_len            = sizeof(csin);
 	client.sock         = sock;
 	client.addr         = (struct sockaddr*)&csin;
-	client.addr_len     = csin_len;
+	client.addr_len     = sizeof(csin);
 
+	// Bind it.
 	if (-1 == bind(sock, (struct sockaddr*)&sin, sizeof(sin))) {
 		perror("couldnt bind socket");
 		return 1;
   }
 
+	// Init the server.
 	server_init();
+
+	// Allocate a buffer.
 	buf = (char*) malloc(sizeof(char) * BUFLEN);
-	// boucle infinie
+
+	// Enter an infinite loop.
 	for (;;) {
+		// Receive from any client.
 		n = client_recv(client, buf, BUFLEN);
 
+		// Slice and copy buffer into a new memory space.
 		data = (char*) malloc(sizeof(char) * n);
 		data[n-1] = '\0';
 		memcpy(data, buf, n-1);
+
+		// Make the server handle the received data.
 		err = server_handle(client, data, n-1);
+
+		// Free the now useless memory.
 		free(data);
+
+		// Close the server if there was an error.
 		if (err) {
 			perror("server fault");
 			break;
     }
   }
 
+	// Free the memory taken by the buffer.
 	free(buf);
+
+	// Clean the server.
 	server_clean();
+
+	// Close the socket.
+	close(sock);
 
 	return 0;
 }
